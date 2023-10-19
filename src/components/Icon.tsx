db897c2ababd5d949c07508aecbbc40a52de7078
Ana Júlia <alvncar@gmail.com>
2023-10-19 12:33:14 -0300

    :truck: chore: mudança no nome do componente para evitar confusão

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -22,7 +22,7 @@ const socialIcons: ISocialIcon[] = [
   },
 ]
 
-export function SocialMediaLinks() {
+export function SocialMediaIcon() {
   return (
     <div className="flex gap-5 justify-center">
       {socialIcons.map((socialIcon, index) => (
